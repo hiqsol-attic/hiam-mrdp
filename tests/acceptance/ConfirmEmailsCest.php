451a1f0bb417ac7b2070bd6fec23e323cddc385b
Andrey Klochok <andreyklochok@gmail.com>
2018-08-20 16:36:03 +0300

    Uncomment the second test

--- tests/acceptance/ConfirmEmailsCest.php
+++ tests/acceptance/ConfirmEmailsCest.php
@@ -14,11 +14,11 @@ class ConfirmEmailsCest
         $I->see('Failed confirm email. Please start over.', ['css' => '.ui-pnotify-text']);
     }
 
-//    public function ensureIGetAnErrorWhenITryToContactConfirmEmail(AcceptanceTester $I)
-//    {
-//        $I->wantTo('check I get an error when I try to visit the contact confirm page without token');
-//        $I->amOnPage('/registration/contact-confirm-email');
-//        $I->waitForElement('.ui-pnotify-text', 10);
-//        $I->see('Failed confirm email. Please start over.', ['css' => '.ui-pnotify-text']);
-//    }
+    public function ensureIGetAnErrorWhenITryToContactConfirmEmail(AcceptanceTester $I)
+    {
+        $I->wantTo('check I get an error when I try to visit the contact confirm page without token');
+        $I->amOnPage('/registration/contact-confirm-email');
+        $I->waitForElement('.ui-pnotify-text', 10);
+        $I->see('Failed confirm email. Please start over.', ['css' => '.ui-pnotify-text']);
+    }
 }
