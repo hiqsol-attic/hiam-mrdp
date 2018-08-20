518f2927b09fdde74312972940c4428f2fe4b23a
Andrey Klochok <andreyklochok@gmail.com>
2018-08-20 11:21:48 +0300

    typo

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Client module for HiPanel
+ * hiam-mrdp module for HiPanel
  *
- * @link      https://github.com/hiqdev/hipanel-module-client
- * @package   hipanel-module-client
+ * @link      https://github.com/hiqdev/hiam-mrdp
+ * @package   hiqdev-hiam-mrdp
  * @license   BSD-3-Clause
  * @copyright Copyright (c) 2015-2018, HiQDev (http://hiqdev.com/)
  */
