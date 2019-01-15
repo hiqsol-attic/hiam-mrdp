579754758b41c54af824febe51c37438e05bd0cd
Andrii Vasyliev <sol@hiqdev.com>
2019-01-15 14:22:39 +0200

    Added default params

---
+++ src/config/params.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * HIAM module for MRDP database compatibility
+ *
+ * @link      https://github.com/hiqdev/hiam-mrdp
+ * @package   hiam-mrdp
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'hipanel.site'  => null,
+    'hiapi.site'    => null,
+    'hiam.site'     => null,
+    'hiam.site'     => null,
+];
