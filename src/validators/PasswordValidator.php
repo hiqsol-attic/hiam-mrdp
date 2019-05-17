dfde040f598ab961cc35811b48ccfb32a078bdec
Andrey Klochok <andreyklochok@gmail.com>
2019-05-17 17:37:45 +0300

    Added PasswordValidator and Client::updateEmail()

---
+++ src/validators/PasswordValidator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace hiam\mrdp\validators;
+
+use hiam\validators\PasswordValidatorInterface;
+use yii\validators\Validator;
+use Yii;
+
+class PasswordValidator extends Validator implements PasswordValidatorInterface
+{
+    /**
+     * @var string login attribute name
+     */
+    public $loginAttribute = 'login';
+
+    /**
+     * @var string password attribute name
+     */
+    public $passwordAttribute = 'current_password';
+
+    public function init()
+    {
+        parent::init();
+        if ($this->message !== null) {
+            return;
+        }
+        $this->message = Yii::t('hiam', 'The current password is incorrect');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        if ($model->{$this->loginAttribute} && $model->{$this->passwordAttribute}) {
+            $valid = Yii::$app->db->createCommand("
+                    SELECT      zc.obj_id
+                    FROM        zclient zc
+                    WHERE       zc.state_id = ANY(state_ids('client', 'ok,active,new'))
+                                AND NOT check_password('', zc.password)
+                                AND login = :login AND check_password(:password, zc.password)
+                ")->bindValues([':login' => $model->{$this->loginAttribute}, ':password' => $model->{$this->passwordAttribute}])->queryScalar();
+            if (!$valid) {
+                $this->addError($model, $attribute, $this->message);
+            }
+        }
+    }
+}
