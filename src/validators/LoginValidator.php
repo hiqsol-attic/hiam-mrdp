17af2a498a9417f169e90a4d9dd19dcb2f4a5940
Andrey Klochok <andreyklochok@gmail.com>
2019-06-20 15:05:02 +0300

    Added LoginValidator

---
+++ src/validators/LoginValidator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace hiam\mrdp\validators;
+
+use hiam\validators\LoginValidatorInterface;
+use yii\validators\Validator;
+use Yii;
+
+class LoginValidator extends Validator implements LoginValidatorInterface
+{
+    /**
+     * @var string login attribute name
+     */
+    public $loginAttribute = 'username';
+
+    /**
+     * @var string password attribute name
+     */
+    public $passwordAttribute = 'current_password';
+
+    public function init()
+    {
+        parent::init();
+        if ($this->message !== null) {
+            return;
+        }
+        $this->message = Yii::t('hiam', 'Incorrect username');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        if ($model->{$this->loginAttribute}) {
+            $valid = Yii::$app->db->createCommand("
+                    SELECT      zc.obj_id
+                    FROM        zclient zc
+                    WHERE       zc.state_id = ANY(state_ids('client', 'ok,active,new'))
+                                AND NOT check_password('', zc.password)
+                                AND login = :login
+                ")->bindValues([':login' => $model->{$this->loginAttribute}])->queryScalar();
+            if (!$valid) {
+                $this->addError($model, $attribute, $this->message);
+            }
+        }
+    }
+}
